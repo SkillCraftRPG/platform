9d0890834e8d8d6bd9fdd2f0e74dd21e6bc6e256
Francis Pion <francispion@hotmail.com>
2026-01-25 14:40:17 -0500

    summary_length

--- src/SkillCraft.Cms.Infrastructure/Configurations/Constants.cs
+++ src/SkillCraft.Cms.Infrastructure/Configurations/Constants.cs
@@ -3,5 +3,5 @@
 internal static class Constants
 {
   public const int MetaDescriptionMaximumLength = 160;
-  public const int SummaryMaximumLength = 160; // TODO(fpion): [SKILLCRAFT-13](https://logitar.atlassian.net/browse/SKILLCRAFT-13)
+  public const int SummaryMaximumLength = 80;
 }
