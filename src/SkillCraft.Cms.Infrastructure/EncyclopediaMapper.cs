5f2f1ef6dbfc48399616b4152d33bc47b9678e76
Francis Pion <francispion@hotmail.com>
2026-01-28 12:48:20 -0500

    infra

---
+++ src/SkillCraft.Cms.Infrastructure/EncyclopediaMapper.cs
@@ -0,0 +1,60 @@
+﻿using Krakenar.Contracts;
+using Krakenar.Contracts.Actors;
+using Logitar;
+using Logitar.EventSourcing;
+using SkillCraft.Cms.Core.Articles.Models;
+using SkillCraft.Cms.Infrastructure.Entities;
+using AggregateEntity = Krakenar.EntityFrameworkCore.Relational.Entities.Aggregate;
+
+namespace SkillCraft.Cms.Infrastructure;
+
+internal class EncyclopediaMapper
+{
+  private readonly Dictionary<ActorId, Actor> _actors = [];
+  private readonly Actor _system = new();
+
+  public EncyclopediaMapper()
+  {
+  }
+
+  public EncyclopediaMapper(IReadOnlyDictionary<ActorId, Actor> actors)
+  {
+    foreach (KeyValuePair<ActorId, Actor> actor in actors)
+    {
+      _actors[actor.Key] = actor.Value;
+    }
+  }
+
+  public ArticleModel ToArticle(ArticleEntity source)
+  {
+    ArticleModel destination = new()
+    {
+      Id = source.Id,
+      Slug = source.Slug,
+      Title = source.Title,
+      MetaDescription = source.MetaDescription,
+      HtmlContent = source.HtmlContent
+    };
+
+    MapAggregate(source, destination);
+
+    return destination;
+  }
+
+  private void MapAggregate(AggregateEntity source, Aggregate destination)
+  {
+    destination.Version = source.Version;
+
+    destination.CreatedBy = FindActor(source.CreatedBy);
+    destination.CreatedOn = source.CreatedOn.AsUniversalTime();
+
+    destination.UpdatedBy = FindActor(source.UpdatedBy);
+    destination.UpdatedOn = source.UpdatedOn.AsUniversalTime();
+  }
+
+  private Actor FindActor(string? id) => TryFindActor(id) ?? _system;
+  private Actor FindActor(ActorId? id) => TryFindActor(id) ?? _system;
+
+  private Actor? TryFindActor(string? id) => TryFindActor(string.IsNullOrWhiteSpace(id) ? null : new ActorId(id));
+  private Actor? TryFindActor(ActorId? id) => id.HasValue && _actors.TryGetValue(id.Value, out Actor? actor) ? actor : null;
+}
---
+++ src/SkillCraft.Cms.Infrastructure/Queriers/ArticleQuerier.cs
@@ -0,0 +1,81 @@
+﻿using Krakenar.Contracts.Actors;
+using Krakenar.Core.Actors;
+using Krakenar.EntityFrameworkCore.Relational.KrakenarDb;
+using Logitar.EventSourcing;
+using Microsoft.EntityFrameworkCore;
+using SkillCraft.Cms.Core.Articles;
+using SkillCraft.Cms.Core.Articles.Models;
+using SkillCraft.Cms.Infrastructure.Entities;
+
+namespace SkillCraft.Cms.Infrastructure.Queriers;
+
+internal class ArticleQuerier : IArticleQuerier
+{
+  private readonly IActorService _actorService;
+  private readonly DbSet<ArticleEntity> _articles;
+
+  public ArticleQuerier(IActorService actorService, EncyclopediaContext encyclopedia)
+  {
+    _actorService = actorService;
+    _articles = encyclopedia.Articles;
+  }
+
+  public async Task<ArticleModel?> ReadAsync(string collection, string path, CancellationToken cancellationToken)
+  {
+    string keyNormalized = Helper.Normalize(collection);
+    string[] slugsNormalized = path.Split('/').Where(segment => !string.IsNullOrWhiteSpace(segment)).Select(Helper.Normalize).ToArray();
+    if (string.IsNullOrEmpty(keyNormalized) || slugsNormalized.Length < 1)
+    {
+      return null;
+    }
+
+    var data = await _articles.AsNoTracking()
+      .Where(x => x.Collection!.KeyNormalized == keyNormalized && slugsNormalized.Contains(x.SlugNormalized))
+      .Select(x => new { x.SlugNormalized, x.ArticleId, x.ParentId })
+      .ToArrayAsync(cancellationToken);
+
+    Dictionary<string, ArticleRelation[]> grouped = data.GroupBy(x => x.SlugNormalized)
+      .ToDictionary(x => x.Key, x => x.Select(ids => new ArticleRelation(ids.ArticleId, ids.ParentId)).ToArray());
+
+    int? articleId = null;
+    foreach (string slugNormalized in slugsNormalized)
+    {
+      if (!grouped.TryGetValue(slugNormalized, out ArticleRelation[]? articles))
+      {
+        return null;
+      }
+
+      int[] articleIds = articles.Where(x => x.ParentId == articleId).Select(x => x.Id).Distinct().ToArray();
+      if (articleIds.Length != 1)
+      {
+        return null;
+      }
+      articleId = articleIds.Single();
+    }
+
+    if (!articleId.HasValue)
+    {
+      return null;
+    }
+
+    ArticleEntity? article = await _articles.AsNoTracking()
+      .Where(x => x.ArticleId == articleId.Value && x.IsPublished)
+      .SingleOrDefaultAsync(cancellationToken);
+    return article is null ? null : await MapAsync(article, cancellationToken);
+  }
+
+  private async Task<ArticleModel> MapAsync(ArticleEntity article, CancellationToken cancellationToken)
+  {
+    return (await MapAsync([article], cancellationToken)).Single();
+  }
+  private async Task<IReadOnlyCollection<ArticleModel>> MapAsync(IEnumerable<ArticleEntity> articles, CancellationToken cancellationToken)
+  {
+    IEnumerable<ActorId> actorIds = articles.SelectMany(article => article.GetActorIds());
+    IReadOnlyDictionary<ActorId, Actor> actors = await _actorService.FindAsync(actorIds, cancellationToken);
+    EncyclopediaMapper mapper = new(actors);
+
+    return articles.Select(mapper.ToArticle).ToList().AsReadOnly();
+  }
+
+  private record ArticleRelation(int Id, int? ParentId);
+}
