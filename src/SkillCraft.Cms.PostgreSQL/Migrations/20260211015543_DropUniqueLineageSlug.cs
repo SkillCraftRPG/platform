0537750cf2815ca5bb237bd797d30351c5a9afe0
Francis Pion <francispion@hotmail.com>
2026-02-10 21:51:17 -0500

    Added missing migration & lineages. (#40)

---
+++ src/SkillCraft.Cms.PostgreSQL/Migrations/20260211015543_DropUniqueLineageSlug.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillCraft.Cms.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropUniqueLineageSlug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Lineages_SlugNormalized",
+                schema: "Rules",
+                table: "Lineages");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lineages_SlugNormalized",
+                schema: "Rules",
+                table: "Lineages",
+                column: "SlugNormalized");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Lineages_SlugNormalized",
+                schema: "Rules",
+                table: "Lineages");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lineages_SlugNormalized",
+                schema: "Rules",
+                table: "Lineages",
+                column: "SlugNormalized",
+                unique: true);
+        }
+    }
+}
